b6f7dc72bc6af06da2655ae944a668e64880a638
Ana Crisan <acrisan@tableau.com>
2021-10-20 16:06:45 -0700

    update app layout

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 ### LIBRARIES ###
-
 # App
 import streamlit as st
 
@@ -9,37 +8,90 @@ import pandas as pd
 import json
 
 #Visualization
+import altair as alt
+from streamlit_vega_lite import altair_component
+
+### LOADING DATA ###
+@st.experimental_memo
+def load_model_card():
+    with open('./data/text_explainer/model_card.json') as f:
+        mc_text= json.load(f)
+    return(mc_text)
 
 
-### LOAD DATA ###
-#model card
-with open('./data/text_explainer/model_card.json') as f:
-  model_card = json.load(f)
+### STREAMLIT APP CONGFIG ###
+st.set_page_config(
+    layout="wide",
+    page_title = "Interactive Model Card"
+)
 
-### STREAMLIT APP ###
-st.set_page_config(layout="wide")
-left_col, right_col = st.columns([4,8])
+#page style with a slightly scary hack
+st.markdown("""
+    <style>
+    /* Side Bar */
+    .css-1outpf7 {
+        background-color:rgb(246 240 240);
+        width:40rem;
+        padding:10px 10px 10px 10px;
+    }
+    /* Main Panel*/
+    .css-18e3th9 {
+        padding:10px 10px 10px 10px;
+    }
+    .css-1ubw6au:last-child{
+        background-color:lightblue;
+    }
+
+    /* Model Panels : element-container */
+    .element-container{
+            border-style:none
+    }
+    </style>
+""",
+unsafe_allow_html=True)
 
 
-with left_col:
-    st.header("Model Information")
-    st.text("Click on (+) for additional information")
-    with st.expander(model_card['model-details']['name']):
-        st.write(model_card['model-details']['short'])
+### STREAMLIT APP LAYOUT###
+#side bar setup
+st.sidebar.title("Model Card")
 
-    with st.expander(model_card['intended-use']['name']):
-        st.write(model_card['intended-use']['short'])
+#quant containers
+quant_div = st.container()
+quant_lcol, quant_rcol = st.columns([6,6])
 
-    with st.expander(model_card['ethical-considerations']['name']):
-        st.write(model_card['ethical-considerations']['short'])
-
-   
-
-with right_col:
-    st.header("Quantitative Analysis")
-    st.text('I am the right column') 
+#data contrainers
+exp_div = st.container()
 
 
-#sidebar
-st.sidebar.header("Add Your Own Data")
-st.sidebar.selectbox('Which number do you like best?',[1,2,3,4,5])
+### STREAMLIT APP CONTENT ###
+#load data
+model_card = load_model_card()
+
+#side panel
+st.sidebar.header(f"{model_card['model-details']['name']}")
+st.sidebar.write(f"{model_card['model-details']['short'][0]}")
+
+with st.sidebar.expander("more details"):
+    st.markdown(f"*{model_card['model-details']['short'][0]}")
+    st.markdown(f"*{model_card['model-details']['short'][1]}")
+
+
+st.header("Ethical Considerations")
+
+#quant containers
+with quant_div:
+    with quant_lcol:
+        st.write("Left Quant Column")
+    with quant_rcol:
+        st.write("Right Quant Column")
+
+#adding own data
+with exp_div:
+    with st.expander("Add your Own Data"):
+        st.write("Add your own data")
+
+
+
+
+
+
