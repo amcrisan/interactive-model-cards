3c7f2e7e76c74c53cb0f6d619a8cb5ad67b737e7
Ana Crisan <acrisan@tableau.com>
2021-10-21 16:52:18 -0700

    adding elements for examples

--- app.py
+++ app.py
@@ -46,29 +46,22 @@ st.markdown("""
     .element-container{
             border-style:none
     }
+
+    div.row-widget.stRadio > div{flex-direction:row;}
     </style>
 """,
 unsafe_allow_html=True)
 
 
 ### STREAMLIT APP LAYOUT###
-#side bar setup
-st.sidebar.title("Model Card")
+#******* side bar setup
+st.sidebar.title("Interactive Model Card")
 
-#quant containers
-quant_div = st.container()
-quant_lcol, quant_rcol = st.columns([6,6])
-
-#data contrainers
-exp_div = st.container()
-
-
-### STREAMLIT APP CONTENT ###
-#load data
+#load model card data
 model_card = load_model_card()
 
 #side panel
-st.sidebar.header(f"{model_card['model-details']['name']}")
+st.sidebar.markdown(f"**{model_card['model-details']['name']}**")
 st.sidebar.write(f"{model_card['model-details']['short'][0]}")
 
 with st.sidebar.expander("more details"):
@@ -76,20 +69,59 @@ with st.sidebar.expander("more details"):
     st.markdown(f"*{model_card['model-details']['short'][1]}")
 
 
-st.header("Ethical Considerations")
 
-#quant containers
-with quant_div:
-    with quant_lcol:
-        st.write("Left Quant Column")
-    with quant_rcol:
-        st.write("Right Quant Column")
+#******* quantaitive analysis
+st.markdown('''**Quantaitive Analysis**''')
+quant_lcol, quant_rcol = st.columns([6,6])
+
+with quant_lcol:
+    st.write("Left Column")
+
+with quant_rcol:
+    st.write("Right Column")
+
+
+#******* Example Layout 
+with st.expander("Add your own examples"):
+    data_src = st.radio('Select Example Source', 
+    ['Text Example','From Training Data','From Your Data'])
+    st.markdown("""---""")
+
+    exp_lcol, exp_mid, exp_rcol = st.columns([5,5,2])
+
+    if data_src == "From Training Data":
+        exp_lcol.write("You training data")
+        exp_mid.write("You training data 2")
+        exp_rcol.write("You training data 3" )
+
+    elif data_src=="From Your Data":
+        exp_lcol.write("Loading your own data")
+        exp_mid.write("Loading your own data 2")
+        exp_rcol.write("Loading your own data 3" )
+    else:
+        exp_lcol.text_input("Add your own example text",'I like you. I love you')
+        exp_mid.write("Example Text 2")
 
 #adding own data
-with exp_div:
-    with st.expander("Add your Own Data"):
-        st.write("Add your own data")
+#with exp_div:
+#    with st.expander("Add your Own Examples"):
+#        data_src = st.radio('Select Example Source', 
+#                       ['Text Example','From Training Data','From Your Data'])
 
+#        if data_src == "From Training Data":
+#            st.write("You training data")
+#        elif data_src=="From Your Data":
+#            st.write("Loading your own data")
+#        else:
+#            st.write("Example Text")
+
+
+#quant containers
+#with quant_div:
+#    with quant_lcol:
+#        st.write("Left Quant Column")
+#    with quant_rcol:
+#        st.write("Right Quant Column")
 
 
 
