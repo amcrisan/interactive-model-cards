78cb59bf0d29340e8bcaffb2219bcc037e0c772a
Ana Crisan <acrisan@tableau.com>
2021-10-22 15:53:57 -0700

    add rg dev bench

--- app.py
+++ app.py
@@ -1,32 +1,61 @@
 ### LIBRARIES ###
-# App
-import streamlit as st
-
-# Data
+# # Data
 import numpy as np
 import pandas as pd
 import json
 
-#Visualization
+# Robustness Gym and Analysis
+import robustnessgym as rg
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+# App & Visualization
+import streamlit as st
 import altair as alt
 from streamlit_vega_lite import altair_component
 
+
 ### LOADING DATA ###
+
+# model card data
 @st.experimental_memo
 def load_model_card():
-    with open('./data/text_explainer/model_card.json') as f:
-        mc_text= json.load(f)
-    return(mc_text)
+    with open("./assets/data/text_explainer/model_card.json") as f:
+        mc_text = json.load(f)
+    return mc_text
+
+
+# robusntess gym from hugging face
+@st.experimental_singleton
+def load_data():
+    # load dev bench
+    devBench = rg.DevBench.load("./assets/data/rg/sst_db.devbench")
+    return devBench
+
+
+# load model
+@st.experimental_singleton
+def load_model():
+    model = rg.HuggingfaceModel(
+        "distilbert-base-uncased-finetuned-sst-2-english", is_classifier=True
+    )
+    return model
+
+
+# loading the dataset
+def load_basic():
+    # load data
+    devBench = load_data()
+    # load model
+    model = load_model()
+    return devBench, model
 
 
 ### STREAMLIT APP CONGFIG ###
-st.set_page_config(
-    layout="wide",
-    page_title = "Interactive Model Card"
-)
+st.set_page_config(layout="wide", page_title="Interactive Model Card")
 
-#page style with a slightly scary hack
-st.markdown("""
+# page style with a slightly scary hack
+st.write(
+    """
     <style>
     /* Side Bar */
     .css-1outpf7 {
@@ -50,17 +79,21 @@ st.markdown("""
     div.row-widget.stRadio > div{flex-direction:row;}
     </style>
 """,
-unsafe_allow_html=True)
-
+    unsafe_allow_html=True,
+)
 
 ### STREAMLIT APP LAYOUT###
-#******* side bar setup
+# ******* side bar setup
 st.sidebar.title("Interactive Model Card")
 
-#load model card data
+# load model card data
 model_card = load_model_card()
 
-#side panel
+with st.spinner():
+    sst_db, model = load_basic()
+
+
+# side panel
 st.sidebar.markdown(f"**{model_card['model-details']['name']}**")
 st.sidebar.write(f"{model_card['model-details']['short'][0]}")
 
@@ -69,61 +102,53 @@ with st.sidebar.expander("more details"):
     st.markdown(f"*{model_card['model-details']['short'][1]}")
 
 
-
-#******* quantaitive analysis
-st.markdown('''**Quantaitive Analysis**''')
-quant_lcol, quant_rcol = st.columns([6,6])
+# ******* quantaitive analysis
+st.markdown("""**Quantaitive Analysis**""")
+quant_lcol, quant_rcol = st.columns([6, 6])
 
 with quant_lcol:
-    st.write("Left Column")
-
+    st.write(sst_db.metrics)
 with quant_rcol:
     st.write("Right Column")
 
 
-#******* Example Layout 
-with st.expander("Add your own examples"):
-    data_src = st.radio('Select Example Source', 
-    ['Text Example','From Training Data','From Your Data'])
+# ******* Example Layout
+with st.expander("Add your own examples", expanded=True):
+    data_src = st.radio(
+        "Select Example Source",
+        ["Text Example", "From Training Data", "From Your Data"],
+    )
     st.markdown("""---""")
 
-    exp_lcol, exp_mid, exp_rcol = st.columns([5,5,2])
+    exp_lcol, exp_mid, exp_rcol = st.columns([5, 5, 2])
 
     if data_src == "From Training Data":
         exp_lcol.write("You training data")
         exp_mid.write("You training data 2")
-        exp_rcol.write("You training data 3" )
+        exp_rcol.write("You training data 3")
 
-    elif data_src=="From Your Data":
+    elif data_src == "From Your Data":
         exp_lcol.write("Loading your own data")
         exp_mid.write("Loading your own data 2")
-        exp_rcol.write("Loading your own data 3" )
+        exp_rcol.write("Loading your own data 3")
     else:
-        exp_lcol.text_input("Add your own example text",'I like you. I love you')
-        exp_mid.write("Example Text 2")
+        user_text = exp_lcol.text_input(
+            "Add your own example text", "I like you. I love you"
+        )
 
-#adding own data
-#with exp_div:
-#    with st.expander("Add your Own Examples"):
-#        data_src = st.radio('Select Example Source', 
-#                       ['Text Example','From Training Data','From Your Data'])
+        # adding user data to the data panel
+        dp2 = rg.DataPanel({"sentence": [user_text], "label": [1]})
+        dp2._identifier = f"User Input - {user_text}"
 
-#        if data_src == "From Training Data":
-#            st.write("You training data")
-#        elif data_src=="From Your Data":
-#            st.write("Loading your own data")
-#        else:
-#            st.write("Example Text")
+        # run prediction
+        model.predict_batch(dp2, ["sentence"])
+        dp2 = dp2.update(
+            lambda x: model.predict_batch(x, ["sentence"]),
+            batch_size=4,
+            is_batched_fn=True,
+            pbar=True,
+        )
 
+        sst_db.add_slices([dp2])
 
-#quant containers
-#with quant_div:
-#    with quant_lcol:
-#        st.write("Left Quant Column")
-#    with quant_rcol:
-#        st.write("Right Quant Column")
-
-
-
-
-
+        exp_mid.write(sst_db.metrics)
